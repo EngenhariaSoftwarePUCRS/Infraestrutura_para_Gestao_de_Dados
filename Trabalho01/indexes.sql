cdadfda2980c16b7fc9c2424e7997bcac575822f
Felipe Freitas <eu@felipefs.dev>
2024-10-08 20:31:33 -0300

    T1 - More Indexes

--- Trabalho01/indexes.sql
+++ Trabalho01/indexes.sql
@@ -45,7 +45,49 @@ CREATE INDEX idx_air_f_departure_char
 CREATE INDEX idx_air_f_arrival_month
     ON AIR_FLIGHTS(EXTRACT(MONTH FROM arrival));
 
+-- SELECT COUNT(DISTINCT flightno) / COUNT(*) FROM AIR_FLIGHTS;
+/*CREATE INDEX idx_air_f_flightno
+    ON AIR_FLIGHTS(flightno);*/
+
+-- SELECT COUNT(DISTINCT airline_id) / COUNT(*) FROM AIR_FLIGHTS;
+CREATE INDEX idx_air_f_airline_id
+    ON AIR_FLIGHTS(airline_id);
+
+-- SELECT COUNT(DISTINCT from_airport_id) / COUNT(*) FROM AIR_FLIGHTS;
+/*CREATE INDEX idx_air_f_from_airport_id
+    ON AIR_FLIGHTS(from_airport_id);*/
+
+-- SELECT COUNT(DISTINCT to_airport_id) / COUNT(*) FROM AIR_FLIGHTS;
+/*CREATE INDEX idx_air_f_to_airport_id
+    ON AIR_FLIGHTS(to_airport_id);*/
+
+-- SELECT COUNT(DISTINCT airplane_id) / COUNT(*) FROM AIR_FLIGHTS;
+/*CREATE INDEX idx_air_f_airplane_id
+    ON AIR_FLIGHTS(airplane_id);*/
+
 -- Create indexes for the AIR_BOOKINGS table
 -- SELECT COUNT(DISTINCT TO_NUMBER(REGEXP_SUBSTR(seat, '^[0-9]+'))) / COUNT(*) FROM AIR_BOOKINGS;
 CREATE INDEX idx_air_bookings_n_regex_seat
     ON AIR_BOOKINGS(TO_NUMBER(REGEXP_SUBSTR(seat, '^[0-9]+')));
+
+-- SELECT COUNT(DISTINCT passenger_id) / COUNT(*) FROM AIR_BOOKINGS;
+CREATE INDEX idx_air_bookings_passenger_id
+    ON AIR_BOOKINGS(passenger_id);
+
+-- SELECT COUNT(DISTINCT flight_id) / COUNT(*) FROM AIR_BOOKINGS;
+CREATE INDEX idx_air_bookings_flight_id
+    ON AIR_BOOKINGS(flight_id);
+
+-- Create indexes for the AIR_AIRPLANES table
+-- SELECT COUNT(DISTINCT airline_id) / COUNT(*) FROM AIR_AIRPLANES;
+CREATE INDEX idx_air_airplanes_airline_id
+    ON AIR_AIRPLANES(airline_id);
+
+-- SELECT COUNT(DISTINCT airplane_type_id) / COUNT(*) FROM AIR_AIRPLANES;
+CREATE INDEX idx_air_a_airplane_type_id
+    ON AIR_AIRPLANES(airplane_type_id);
+
+-- Create indexes for the AIR_FLIGHTS_SCHEDULES table
+-- SELECT COUNT(DISTINCT airline_id) / COUNT(*) FROM AIR_FLIGHTS_SCHEDULES;
+CREATE INDEX idx_air_f_s_airline_id
+    ON AIR_FLIGHTS_SCHEDULES(airline_id);
