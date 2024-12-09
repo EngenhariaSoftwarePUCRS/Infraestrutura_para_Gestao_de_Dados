86fc35c67b6ff941a98a360f7ef50466ade5264d
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-12-09 11:37:38 -0300

    T3 - 1000 Lines of Sample Inserts

---
+++ Trabalho03/sql/Insert_instrument_type.sql
@@ -0,0 +1,8 @@
+INSERT INTO instrument_type (id, name, parent, child_order, description, gid) VALUES
+('1', 'Wind instrument', NULL, '1', NULL, '876464a8-e74f-3f40-9bd3-637d2b1743ae'),
+('2', 'String instrument', NULL, '2', NULL, 'cc00f97f-cf3d-3ae2-9163-041cb1a0d726'),
+('3', 'Percussion instrument', NULL, '3', NULL, '68fa2525-4c23-3386-bb81-e84994342e9a'),
+('4', 'Electronic instrument', NULL, '4', NULL, '98df6cec-95e5-3cbc-9a2d-7ea6c8be6f3c'),
+('5', 'Other instrument', NULL, '5', NULL, '01ba5777-02dd-347d-94a1-73e3db00215d'),
+('6', 'Ensemble', NULL, '99', 'A standard grouping of instruments often played together, like a string quartet', '98d6049d-3823-41ef-af2e-c13c214a787c'),
+('7', 'Family', NULL, '98', 'A grouping of related but different instruments, like the different violin-like instruments', '7c9126a3-32a8-4a46-ba46-b21c83874827');
